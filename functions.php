5067b71aee8d2e7fad9fb83a7c69b971eebff17a
jbmxworld@gmail.com <jbmxworld@gmail.com>
2020-11-21 14:14:28 -0500

    structure basic files

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Functions File
+ * 
+ * @package Rigorous
+ */
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Main template
+ * 
+ * @package Rigorous
+ */
+get_header();
+?>
+
+<?php
+get_footer();
